a76e6c2818ffc9a8cd47ef0c7c98cc800771cc72
Danny Tunney <dtun@me.com>
2024-09-22 18:48:42 -0700

    Intercept and Mock Server-Side HTTP Requests

--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -1,1 +1,35 @@
-export const handlers = [];
+import { http } from "msw";
+
+export const handlers = [
+  http.get("https://api.example.com/movies/featured", () => {
+    return new Response(
+      JSON.stringify([
+        {
+          id: 1,
+          title: "Inception",
+          slug: "inception",
+          category: "Science Fiction",
+          imageUrl: "https://example.com/inception.jpg",
+          releasedAt: "2010-07-16",
+          description:
+            "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+        },
+        {
+          id: 2,
+          title: "The Dark Knight",
+          slug: "the-dark-knight",
+          category: "Action",
+          imageUrl: "https://example.com/the-dark-knight.jpg",
+          releasedAt: "2008-07-18",
+          description:
+            "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept",
+        },
+      ]),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }),
+];
