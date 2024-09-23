5e93ffa8fbd6cab892f46b6ccd28dee6fac6f540
Danny Tunney <dtun@me.com>
2024-09-22 19:40:15 -0700

    Intercept and Mock Client-Side HTTP Requests

--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -50,4 +50,7 @@ export const handlers = [
 
     return new HttpResponse("Not found", { status: 404 });
   }),
+  http.get("api/recommendations", () => {
+    return HttpResponse.json(movies.slice(0, 2));
+  }),
 ];
---
+++ app/routes/api.recommendations.ts
@@ -0,0 +1,7 @@
+import { type LoaderArgs } from "@remix-run/node";
+/**
+ * GET /api/recommendations
+ */
+export function loader({ request: _request }: LoaderArgs) {
+  return [];
+}
