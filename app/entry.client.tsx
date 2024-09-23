a29182133766e7b201c9a597b0eb40b0e7fd4eb5
Danny Tunney <dtun@me.com>
2024-09-22 19:42:00 -0700

    Clean up client entry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,10 +3,11 @@ import { startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 async function enableApiMocking() {
-  if (process.env.NODE_ENV !== "development") return;
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
 
   const { worker } = await import("./mocks/browser");
-
   await worker.start();
 }
 
