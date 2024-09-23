b5c11eb4e15f1099020f16d0647e3336f962b8d2
Danny Tunney <dtun@me.com>
2024-09-22 19:21:54 -0700

    Add line breaks

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,7 +4,9 @@ import { hydrateRoot } from "react-dom/client";
 
 async function enableApiMocking() {
   if (process.env.NODE_ENV !== "development") return;
+
   const { worker } = await import("./mocks/browser");
+
   await worker.start();
 }
 
